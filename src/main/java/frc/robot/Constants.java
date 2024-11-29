78ce2c987a2101ec49504b7530ace0dc2a6c1e97
shdalton <stephane.h.dalton@gmail.com>
2024-11-28 21:48:46 -0500

    WPI vision speakerlock

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -5,4 +5,20 @@
 package frc.robot;
 
 /** Add your docs here. */
-public final class Constants {}
+public final class Constants {
+  public static final double stickDeadband = 0.1;
+
+  public static class VisionConstants {
+    public static long kPositionCoalescingTime = 100 * 1000; // 100 ms in microseconds
+    public static int kRedSpeakerTag = 4;
+    public static int kBlueSpeakerTag = 7;
+    public static Integer kSpeakerIndex[] = {kRedSpeakerTag, kBlueSpeakerTag};
+    public static int kRedAmpTag = 5;
+    public static int kBlueAmpTag = 6;
+    public static Integer kAmpIndex[] = {kRedAmpTag, kBlueAmpTag};
+    public static double kAmpRiseElevatorDistance = 0.5;
+    public static double kAmpShootingDistance = 0.1;
+    public static double kShooterCameraPitch = 0.1309; // 7.5deg;
+    public static double kCameraHeight = 0.432; // 43.2 cm
+  }
+}
