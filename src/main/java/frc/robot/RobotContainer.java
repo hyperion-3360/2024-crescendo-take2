9a52ff896f6f401a27539bd49677705f1ffed4a0
elliotA <elliot.alloiteau@icloud.com>
2024-12-09 16:40:10 -0500

    working LEDs
    red, green, blue, teal, orange and yellow works but not rainbow

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,15 +4,28 @@
 
 package frc.robot;
 
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import frc.robot.subsystems.LEDs;
 
 public class RobotContainer {
+  // The robot's subsystems and commands are defined here...
+  private final LEDs m_LEDs = new LEDs();
+
   public RobotContainer() {
     configureBindings();
   }
 
-  private void configureBindings() {}
+  private void configureBindings() {
+    SmartDashboard.putData("rainbow", m_LEDs.rainbow());
+    SmartDashboard.putData("red", m_LEDs.red());
+    SmartDashboard.putData("green", m_LEDs.green());
+    SmartDashboard.putData("blue", m_LEDs.blue());
+    SmartDashboard.putData("teal", m_LEDs.teal());
+    SmartDashboard.putData("yellow", m_LEDs.yellow());
+    SmartDashboard.putData("orange", m_LEDs.orange());
+  }
 
   public Command getAutonomousCommand() {
     return Commands.print("No autonomous command configured");
